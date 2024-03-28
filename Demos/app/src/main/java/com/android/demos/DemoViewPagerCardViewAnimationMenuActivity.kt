593cd071f2b2cd14dcbd7580915a95eb955d0c40
hufei <909867044@qq.com>
2024-03-28 20:29:37 +0800

    优化 appt bug

--- Demos/app/src/main/java/com/android/demos/DemoViewPagerCardViewAnimationMenuActivity.kt
+++ Demos/app/src/main/java/com/android/demos/DemoViewPagerCardViewAnimationMenuActivity.kt
@@ -9,22 +9,24 @@ import com.android.launcher.librarys.controller.Ctrl
 import com.android.launcher.librarys.adapter.CardAdapter
 import com.android.launcher.librarys.util.DensityUtil
 import kotlin.math.roundToInt
+import com.android.launcher.librarys.dc.R
 
 class DemoViewPagerCardViewAnimationMenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_demo)
+        setContentView(com.android.demos.R.layout.activity_demo)
         initViewPager()
     }
 
     private fun initViewPager() {
         val cardAdapter =
-            com.android.launcher.librarys.adapter.CardAdapter(arrayOf(R.mipmap.ic_menu_classic, R.mipmap.ic_menu_sport, R.mipmap.ic_menu_tech, R.mipmap.ic_menu_map, R.mipmap.ic_menu_maintain))
-        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
-        val padding = (com.android.launcher.librarys.util.DensityUtil.getScreenWidth(this) / 2.65f).roundToInt()
-        com.android.launcher.librarys.controller.Ctrl.Functions.initViewPagerMenu(cardAdapter, viewPager, viewPager.getChildAt(0), padding) { page, pos ->
-            val anim = if (pos == 0F) R.anim.scale_anim_menu_bigger else R.anim.scale_anim_menu_smaller
+            CardAdapter(arrayOf(R.mipmap.ic_menu_classic, R.mipmap.ic_menu_sport, R.mipmap.ic_menu_tech, R.mipmap.ic_menu_map, R.mipmap.ic_menu_maintain))
+        val viewPager = findViewById<ViewPager2>(com.android.demos.R.id.view_pager)
+        val padding = (DensityUtil.getScreenWidth(this) / 2.65f).roundToInt()
+        Ctrl.Functions.initViewPagerMenu(cardAdapter, viewPager, viewPager.getChildAt(0), padding) { page, pos ->
+            val anim = if (pos == 0F)
+                R.anim.scale_anim_menu_bigger else R.anim.scale_anim_menu_smaller
             AnimationUtils.loadAnimation(this, anim).apply {
                 this.interpolator = AnticipateInterpolator()
                 page.startAnimation(this)
