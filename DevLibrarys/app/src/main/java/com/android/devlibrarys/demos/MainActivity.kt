3a2765e97bd9856294f87c823563a63b08747029
hufei <909867044@qq.com>
2024-03-28 21:23:48 +0800

    专门测试 demo 并封装成 library

---
+++ DevLibrarys/app/src/main/java/com/android/devlibrarys/demos/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.android.devlibrarys.demos
+
+class MainActivity {
+}
---
+++ DevLibrarys/app/src/main/java/com/android/devlibrarys/demos/ViewPager2DemoActivity.kt
@@ -0,0 +1,34 @@
+package com.android.devlibrarys.demos
+
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.AnticipateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import dc.android.libraries.R
+import dc.android.libraries.adapter.CardAdapter
+import dc.android.libraries.controller.Ctrl
+import dc.android.libraries.util.DensityUtil
+import kotlin.math.roundToInt
+
+class ViewPager2DemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.android.devlibrarys.R.layout.activity_view_pager)
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        val cardAdapter = CardAdapter(arrayOf(R.mipmap.ic_menu_classic, R.mipmap.ic_menu_sport, R.mipmap.ic_menu_tech, R.mipmap.ic_menu_map, R.mipmap.ic_menu_maintain))
+        val viewPager = findViewById<ViewPager2>(com.android.devlibrarys.R.id.view_pager)
+        val padding = (DensityUtil.getScreenWidth(this) / 2.65f).roundToInt()
+        Ctrl.Functions.initViewPagerMenu(cardAdapter, viewPager, viewPager.getChildAt(0), padding) { page, pos ->
+            val anim = if (pos == 0F) R.anim.scale_anim_menu_bigger else R.anim.scale_anim_menu_smaller
+            AnimationUtils.loadAnimation(this, anim).apply {
+                this.interpolator = AnticipateInterpolator()
+                page.startAnimation(this)
+            }
+        }
+    }
+}
